adce7974658d52792d13b1a618201eaba68f9741
Jaron-Lane <jaronwohlwend@icloud.com>
2021-03-16 14:41:36 -0500

    Created Plant.js and PlantList.js. Untested.

---
+++ src/components/plants/Plant.js
@@ -0,0 +1,10 @@
+import { React } from "react";
+import "./Plant.css";
+
+export default ({ plant }) => (
+    <section className="plant">
+        <h3 className="plant__nick_name">{ plant.nick_name }</h3>
+        <div className="plant__title">{ plant.title }</div>
+        <div className="plant__watering_frequency">{ plant.watering_frequency }</div>
+    </section>
+)
