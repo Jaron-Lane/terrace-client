a8d53ef02b0156736f0d40d4dc1481d01ee08149
Jaron-Lane <jaronwohlwend@icloud.com>
2021-03-16 14:05:11 -0500

    Initial setup of PlantProvider

---
+++ src/components/plants/PlantProvider.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+export const PlantContext = React.createContext()
+
+export const PlantProvider = (props) => {
+    const [ plants, setPlants ] = useState([])
+
+    const getPlants = () => {
+        return fetch("http://localhost:8000/plants")
+            .then(res => res.json())
+            .then(setPlants)
+    }
+
+    return (
+        <PlantContext.Provider value={{
+            plants, setPlants, getPlants
+        }}>
+            {props.children}
+        </PlantContext.Provider>
+    )
+}
