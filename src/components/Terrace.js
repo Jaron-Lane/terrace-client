0ad18af01eaf3084549e5878fb6538c2848cb48e
Jaron-Lane <jaronwohlwend@icloud.com>
2021-03-16 11:12:35 -0500

    Rendering terrace.js to the dom

---
+++ src/components/Terrace.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Terrace.css"
+
+export const Terrace = () => (
+    <>
+        <h2>Terrace App</h2>
+        <small>Helping you remember all your plant needs</small>
+        <address>
+            <div>Hoping you download us</div>
+        </address>
+    </>
+)
