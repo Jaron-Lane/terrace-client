1da37dab2e0ae98991c1d8b3ef95063c7e0bce64
Jaron-Lane <jaronwohlwend@icloud.com>
2021-03-17 10:43:51 -0500

    ApplicationViews updated with /plants route

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,19 @@
 import { React } from "react";
 import { Route } from "react-router-dom";
-import { PlantProvider } from "./plants/PlantProvider.js";
-import { PlantList } from "./plants/PlantList.js";
+import { PlantProvider } from "./plants/PlantProvider";
+import { PlantList } from "./plants/PlantList";
 
 export const ApplicationViews = () => {
     return (
         <>
             <PlantProvider>
                 <Route exact path="/">
+                    <PlantList />
+                </Route>
+            </PlantProvider>
+
+            <PlantProvider>
+                <Route path="/plants">
                     <PlantList />
                 </Route>
             </PlantProvider>
