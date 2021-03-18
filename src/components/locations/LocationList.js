115732f17ef51ec9bba5305c7a8d08b2b4b1913b
Jaron-Lane <jaronwohlwend@icloud.com>
2021-03-17 20:37:44 -0500

    Updated LocationList with button pathname

--- src/components/locations/LocationList.js
+++ src/components/locations/LocationList.js
@@ -1,9 +1,11 @@
 import { React, useContext, useEffect } from "react";
+import { useHistory } from "react-router";
 import { Location } from "./Location";
 import { LocationContext } from "./LocationProvider";
 import "./Locations.css"
 
 export const LocationList = () => {
+    const history = useHistory()
     const { locations, getLocations } = useContext(LocationContext)
 
     useEffect(() => {
@@ -12,7 +14,10 @@ export const LocationList = () => {
 
     return (
         <div style={{ marginTop: "2rem" }}>
-            <button>Add New Location</button>
+            <button className="btn btn-add-location"
+                onClick={() => {
+                    history.push({ pathname: "/locations/create" })
+                }}>Add New Location</button>
             <div className="locations">
                 {
                     locations.map(location => <Location key={location.id} location={location} />)
