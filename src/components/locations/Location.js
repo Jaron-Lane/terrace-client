6e0a0035d2e75fe6dff3bba3a36e8cb319281e03
Jaron-Lane <jaronwohlwend@icloud.com>
2021-03-18 19:39:55 -0500

    Added Delete and Edit Buttons

--- src/components/locations/Location.js
+++ src/components/locations/Location.js
@@ -5,5 +5,11 @@ export const Location = ({ location }) => (
     <section className="location">
         <h3 className="location__name">{ location.name }</h3>
         <div className="location__lighting">{ location.lighting }</div>
+        <button>
+            Edit
+        </button>
+        <button>
+            Delete
+        </button>
     </section>
 )
